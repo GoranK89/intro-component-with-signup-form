cd4c701415184ccbf3f5f031e2d59ab1b70fcd98
Goran Kolar <86071804+GoranK89@users.noreply.github.com>
2023-01-13 13:56:08 +0100

    added input hook & useReducer

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,45 @@
+import useInput from "../../hooks/use-input";
+
 import CtaBanner from "../CtaBanner";
+import InputErrorMsg from "./InputErrorMsg";
 
 const Form = () => {
+  const {
+    value: nameValue,
+    isValid: nameIsValid,
+    hasError: nameError,
+    onChangeHandler: nameInputChangeHandler,
+    onBlurHandler: nameInputBlurHandler,
+    reset,
+  } = useInput((value) => value.trim() !== "");
+
+  let formIsValid = false;
+  if (nameIsValid) formIsValid = true;
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid) return;
+    console.log(nameValue);
+    reset();
+  };
+
   return (
     <div className="form__container">
       <CtaBanner />
-      <form className="form">
-        <input type="text" placeholder="First Name" />
+      <form className="form" onSubmit={onSubmitHandler}>
+        <input
+          type="text"
+          placeholder="First Name"
+          value={nameValue}
+          onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
+        />
+        {nameError && <InputErrorMsg errorText="Check name" />}
         <input type="text" placeholder="Last Name" />
         <input type="email" placeholder="Email Adress" />
         <input type="password" placeholder="Password" />
-        <button>Claim your free trial</button>
+        <button disabled={!formIsValid}>Claim your free trial</button>
         <p className="form-terms">
           <span className="form-terms--text">
             By clicking this button you are agreeing to our
---
+++ src/components/Form/InputErrorMsg.jsx
@@ -0,0 +1,5 @@
+const InputErrorMsg = (props) => {
+  return <span className="error-message">{props.errorText}</span>;
+};
+
+export default InputErrorMsg;
