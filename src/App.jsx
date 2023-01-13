64728d7a0a50cc08fc8bc33425394e19f72f2087
Goran Kolar <86071804+GoranK89@users.noreply.github.com>
2023-01-13 11:22:33 +0100

    styling first pass

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import HeroText from "./components/HeroText";
-import CtaBanner from "./components/CtaBanner";
 import Form from "./components/Form/Form";
 
 function App() {
   return (
     <main>
       <HeroText />
-      <CtaBanner />
       <Form />
     </main>
   );
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,20 +1,25 @@
+import CtaBanner from "../CtaBanner";
+
 const Form = () => {
   return (
-    <form className="form">
-      <input type="text" placeholder="First Name" />
-      <input type="text" placeholder="Last Name" />
-      <input type="email" placeholder="Email Adress" />
-      <input type="password" placeholder="Password" />
-      <button>Claim your free trial</button>
-      <p className="form-terms">
-        <span className="form-terms--text">
-          By clicking this button you are agreeing to our
-        </span>
-        <a href="#" className="form-terms--link">
-          Terms and Services
-        </a>
-      </p>
-    </form>
+    <div className="form__container">
+      <CtaBanner />
+      <form className="form">
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+        <input type="email" placeholder="Email Adress" />
+        <input type="password" placeholder="Password" />
+        <button>Claim your free trial</button>
+        <p className="form-terms">
+          <span className="form-terms--text">
+            By clicking this button you are agreeing to our
+          </span>
+          <a href="#" className="form-terms--link">
+            Terms and Services
+          </a>
+        </p>
+      </form>
+    </div>
   );
 };
 
