08ffe568a8ed72f22cac446162acd1a0974b98af
Goran Kolar <86071804+GoranK89@users.noreply.github.com>
2023-01-13 09:58:46 +0100

    added the html structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import HeroText from "./components/HeroText";
+import CtaBanner from "./components/CtaBanner";
+import Form from "./components/Form/Form";
+
 function App() {
-  return <div></div>;
+  return (
+    <main>
+      <HeroText />
+      <CtaBanner />
+      <Form />
+    </main>
+  );
 }
 
 export default App;
