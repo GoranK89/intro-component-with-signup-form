95665a18d64f484f49dc6c4b56dad1bb47ffb16d
Goran Kolar <86071804+GoranK89@users.noreply.github.com>
2023-01-15 21:42:56 +0100

    tweaked and styled validation

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -48,6 +48,7 @@ const Form = () => {
     event.preventDefault();
 
     if (!formIsValid) return;
+
     console.log(
       `${nameValue}\n${surnameValue}\n${emailValue}\n${passwordValue}`
     );
@@ -61,38 +62,58 @@ const Form = () => {
     <div className="form__container">
       <CtaBanner />
       <form className="form" onSubmit={onSubmitHandler}>
-        <input
-          type="text"
-          placeholder="First Name"
-          value={nameValue}
-          onChange={nameInputChangeHandler}
-          onBlur={nameInputBlurHandler}
-        />
-        {nameError && <InputErrorMsg errorText="Check name" />}
-        <input
-          type="text"
-          placeholder="Last Name"
-          value={surnameValue}
-          onChange={surnameInputChangeHandler}
-          onBlur={surnameInputBlurHandler}
-        />
-        {surnameError && <InputErrorMsg errorText="Check surname" />}
-        <input
-          type="text"
-          placeholder="Email Adress"
-          value={emailValue}
-          onChange={emailInputChangeHandler}
-          onBlur={emailInputBlurHandler}
-        />
-        {emailError && <InputErrorMsg errorText="Check email" />}
-        <input
-          type="password"
-          placeholder="Password"
-          value={passwordValue}
-          onChange={passwordInputChangeHandler}
-          onBlur={passwordInputBlurHandler}
-        />
-        {passwordError && <InputErrorMsg errorText="Check password" />}
+        <div className="form__input-box">
+          <input
+            className={nameError ? "error-border" : ""}
+            type="text"
+            placeholder={nameError || "First Name"}
+            value={nameValue}
+            onChange={nameInputChangeHandler}
+            onBlur={nameInputBlurHandler}
+          />
+          {nameError && (
+            <InputErrorMsg errorText="First Name cannot be empty" />
+          )}
+        </div>
+        <div className="form__input-box">
+          <input
+            className={surnameError ? "error-border" : ""}
+            type="text"
+            placeholder={surnameError || "Last Name"}
+            value={surnameValue}
+            onChange={surnameInputChangeHandler}
+            onBlur={surnameInputBlurHandler}
+          />
+          {surnameError && (
+            <InputErrorMsg errorText="Last Name cannot be empty" />
+          )}
+        </div>
+        <div className="form__input-box">
+          <input
+            className={emailError ? "error-border" : ""}
+            type="text"
+            placeholder={emailError || "Email Adress"}
+            value={emailValue}
+            onChange={emailInputChangeHandler}
+            onBlur={emailInputBlurHandler}
+          />
+          {emailError && (
+            <InputErrorMsg errorText="Looks like this is not an email" />
+          )}
+        </div>
+        <div className="form__input-box">
+          <input
+            className={passwordError ? "error-border" : ""}
+            type="password"
+            placeholder={passwordError || "Password"}
+            value={passwordValue}
+            onChange={passwordInputChangeHandler}
+            onBlur={passwordInputBlurHandler}
+          />
+          {passwordError && (
+            <InputErrorMsg errorText="Password cannot be empty" />
+          )}
+        </div>
         <button disabled={!formIsValid}>Claim your free trial</button>
         <p className="form-terms">
           <span className="form-terms--text">
